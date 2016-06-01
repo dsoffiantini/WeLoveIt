0f3e4adb8de2012741bba45146e876b81e100033
Daniel Soffiantini <Daniel Soffiantini>
2016-06-01 14:03:42 -0600

    1

---
+++ scripts/controller.js
@@ -0,0 +1,18 @@
+angular.module("app").controller("controller", function($scope) {
+
+  $scope.images = [
+    {
+      name: "Space",
+      url: "https://pixabay.com/static/uploads/photo/2015/02/04/17/18/space-624054_960_720.jpg",
+      likes: 0,
+      dislikes: 0
+    },
+
+    {
+      name: "Space",
+      url: "https://pixabay.com/static/uploads/photo/2015/02/04/17/18/space-624054_960_720.jpg",
+      likes: 0,
+      dislikes: 0
+    }
+  ]
+});
